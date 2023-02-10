af8ca823ed218d794cef4ad7a0fc55850bf0d241
Kyoorim <kyoorim88@gmail.com>
2023-02-10 13:59:57 +0900

    hotjar 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { authService } from "../config";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "../styles/styles.css";
 import type { AppProps } from "next/app";
+import { hotjar } from "react-hotjar";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [init, setInit] = useState<Boolean>(false);
@@ -12,6 +13,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   const [userObj, setUserObj] = useState<Object | null>(null);
 
   useEffect(() => {
+    hotjar.initialize(3341604, 6);
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
